6c76e5870762b0d99ebc2643642c7a53605f78bf
drexl Spivey <drexl@little-beak.com>
2022-08-19 00:42:41 +0200

    about to clean code, and start new approach - this should work.

--- CryptoAdd-on/background.js
+++ CryptoAdd-on/background.js
@@ -1,5 +1,4 @@
 console.log("1. background.js loaded.");
-
 //This imports the Stanford JavaScript Cryptography Library into this file.
 var imported = document.createElement('script');
 imported.src = 'sjcl.js';
@@ -13,7 +12,7 @@ async function blockingPopup() {
 		//first async -> get window ID
 		try {
 			await messenger.windows.get(popupId);
-			console.log("2. First try reached.");
+			console.log("3. First try reached.");
 		} catch (e) {
 			//window does not exist, assume closed
 			console.log("X. Second try reached. This is never reached, probably can remove?");
@@ -21,7 +20,7 @@ async function blockingPopup() {
 		}
 		//end first async - with either have window id, or no window exists.
 		return new Promise(resolve => {
-			console.log("3. First promise is ready to resolve -- when compose button 'Insecure' is pressed.");
+			console.log("4. First promise is ready to resolve -- loaded when Insecure pressed.");
 			/* let bodyText = messenger.compose.getComposeDetails(tab.id);
 			console("ok this is it! " + bodyText.body); */
 			//on resolved promise -> close clicked button
@@ -30,21 +29,24 @@ async function blockingPopup() {
 				//get we get the body here? -- this seems like the right spot, need to get something other than 
 				//undefined. This is where I want it!
 
-				console.log("7. Random text to encrypt is: " + details.body);
+				
 				
 				//TODO **** NEED PASSWORD HERE!! *****
 				//above all works, now just this part -- //TODO How do I get the password here?
-				console.log("6.5. password entered is: " + password);
+				//currently, break here
+				console.log("8. password entered is: " + password);
 
 				//this works when uncommented.
 				//console.log("10. Encrypt now! " + sjcl.encrypt(password, details.body));
+				
+				console.log("9. Random text to encrypt is: " + details.body);
 
-				console.log("8. Removing window.  This is activated if a Cancel or Encrypt button is pressed.");
+				console.log("10. Removing window.  This is activated if a Cancel or Encrypt button is pressed.");
 				if (popupId == closedId) {
 					messenger.windows.onRemoved.removeListener(windowRemoveListener);
 					messenger.runtime.onMessage.removeListener(messageListener);
 					//this resolves the promise
-					console.log("9. All windows closed.");
+					console.log("11. All windows closed.");
 					resolve(popupCloseMode);
 				}
 			}
@@ -67,17 +69,23 @@ async function blockingPopup() {
 	// this shows either encrypt or cancel - popup is closed (the popup.)
 	//hmm this seems to be done way at the end.
 	let rv = await popupClosePromise(myPopup.id, "cancel");
-	console.log("10. This is rv: " + rv);
+	console.log("12. This is rv: " + rv);
 }
 
 async function getBodyText(tab) {
 	//setting details
 	details = await messenger.compose.getComposeDetails(tab.id);
-	console.log("1.5? Eh. This is the body text from the getBodyText function: " + details.body);
+	console.log("2. This is the body text from the getBodyText function: " + details.body);
 }
+
+async function getPasswordFromPopup() {
+	const password = await messenger.runtime.onMessage.addListener(password);
+}
+
 
 //listener to trigger the popup - event listener? or listener? the difference?
 messenger.composeAction.onClicked.addListener(blockingPopup);
 messenger.composeAction.onClicked.addListener(getBodyText);
+messenger.composeAction.onClicked.addListener(getPasswordFromPopup);
 
 
--- CryptoAdd-on/popup.js
+++ CryptoAdd-on/popup.js
@@ -5,15 +5,19 @@ async function notifyMode(event) {
 
 
     //this works, why can't I send it to background??
-    password = await document.getElementById("password_ok").value;
+    var password = await document.getElementById("password_ok").value;
     console.log("6. This gets the password " + password);
 
+    await messenger.runtime.sendMessage({
+        popupCloseMode: event.target.getAttribute("value")
+    });
+    console.log("6.5. This gets the password " + password);
     
     //Ah! this is the data (encrypt or cancel!)
-    console.log("5. An Ok/Encrypt or Cancel is pressed: " + event.target.getAttribute("data"));
+    console.log("7. An Ok/Encrypt or Cancel is pressed: " + event.target.getAttribute("data"));
     await messenger.runtime.sendMessage({ popupCloseMode: event.target.getAttribute("data") });
 
-    
+    //await - waits for a promise to fulfill
     let win = await messenger.windows.getCurrent();
     messenger.windows.remove(win.id);
 }
@@ -22,6 +26,6 @@ async function onLoad() {
     document.getElementById("button_ok").addEventListener("click", notifyMode);
     document.getElementById("button_cancel").addEventListener("click", notifyMode);
     //the page is loaded, on the "insecure" click.
-    console.log("4. Ok, the popup.js is loaded.");
+    console.log("5. Ok, the popup.js is loaded.");
 }
 
