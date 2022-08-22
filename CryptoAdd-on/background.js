84640bcb1eb4a08e628ca504f5d65ad41a502789
drexl Spivey <drexl@little-beak.com>
2022-08-22 18:13:25 +0200

    getting closer.

--- CryptoAdd-on/background.js
+++ CryptoAdd-on/background.js
@@ -10,71 +10,87 @@ async function blockingPopup() {
 	async function popupClosePromise(popupId, defaultPopupCloseMode) {
 	
 		try {
-			// I believe this is the windowId for the popup window.
+			// Mozilla: details about a window, given it's Id. The details
+			// are passed into a callback. messenger.windows.get(popupId) is an object Promise
 			await messenger.windows.get(popupId);
-			console.log("5. ...");
+			console.log("5. Getting details about the popup window: " + messenger.windows.get(popupId));
 		} catch (e) {
 			//window does not exist, assume closed
-			console.log("X. Second try reached. This should not be reached.");
+			console.log("X. Second try reached. This should not be reached, but the" +
+			"popup I want is gone.");
 			return defaultPopupCloseMode;
 		}
 		//end first async - with either have window id, or no window exists.
 		return new Promise(resolve => {
 
 			//on click - this will get the value from the "data"
-			console.log("4. new Promise is ready to resolve -- loaded when Insecure pressed.");
+			console.log("6. Promise is ready to resolve to ??");
 
 			/* let bodyText = messenger.compose.getComposeDetails(tab.id);
 			console("ok this is it! " + bodyText.body); */
 			//on resolved promise -> close clicked button
 
+			//this variable sets popupCloseMode to defaultPopupCloseMode
+			//why?
+			
 			let popupCloseMode = defaultPopupCloseMode;
+
+			//note this function is not async and all it's content fires through
+			//one parameter..
 			function windowRemoveListener(closedId) {
+				/* This function only checks to see if popupId and closeId are the same
+				if they are, it.. */
 
 			
 				//TODO **** NEED PASSWORD HERE!! *****
 				//above all works, now just this part -- //TODO How do I get the password here?
 				//TODO __ currently, break here -- undefined.
 				//create a async -> resolve to get password here? what is syntax?
-				
-
-				console.log("6.2 password entered is: ");
+				var pw = popupClosePromise(myPopup.id, "cancel");
+				console.log("11. This is pw: " + pw);
 
 				//this works when uncommented.
 				//console.log("10. Encrypt now! " + sjcl.encrypt(password, details.body));
 				
-				console.log("9. Random text to encrypt is: " + details.body);
+				console.log("12. Random text to encrypt is: " + details.body);
 
-				console.log("10. Removing window.  This is activated if a Cancel or Encrypt button is pressed.");
+				console.log("13. Removing window.  This is activated if a Cancel or Encrypt button is pressed.");
 				console.log("popupId and closedId " + popupId + ", " + closedId);
 
 				if (popupId == closedId) {
 
-
+					//"windows.onRemoved" fired when the window is closed.
+					//depreciated? removes the specified listener 
 					messenger.windows.onRemoved.removeListener(windowRemoveListener);
+					// similarly, like above..but at least..
+					//runtime.onmessage = use this event to listen for messages
+					//from another part of your extension.
 					messenger.runtime.onMessage.removeListener(messageListener);
 					//this resolves the promise
-					console.log("11. All windows closed.");
+					console.log("14. All popup windows closed.");
 					resolve(popupCloseMode);
 				}
 			}
 
-
+			//custom made function that takes up to 2 arguments.
 			function messageListener(request, sender, sendResponse) {
 				console.log("sender.tab.windowID " + sender.tab.windowId);
 				console.log("popupId, request, request.popupCloseMode  " + popupId + ", " + request + ", " + request.popupCloseMode);
 				if (sender.tab.windowId == popupId && request && request.popupCloseMode) {
+					//if all the above true, then the popup window is closed.
+					console.log("closing Popup!")
 					popupCloseMode = request.popupCloseMode;
 				}
 			}
 
-			
+			//listen for messages, adds a callback when invoked
 			messenger.runtime.onMessage.addListener(messageListener);
+			//fires when the popup is closed, removes the listener
 			messenger.windows.onRemoved.addListener(windowRemoveListener);
 		});
 	}
 
-	console.log("3. Insecure button cliekd, about to kick start the popup! Namely, popup.html");
+	console.log("3. Insecure button clicked, about to kick start the popup! Namely, popup.html");
 	let myPopup = await messenger.windows.create({
 		 url: "popup.html",
 		 type: "popup",
@@ -85,8 +101,23 @@ async function blockingPopup() {
 	//hmm this seems to be done way at the end.
 	
 	//return value? I guess
-	let rv = await popupClosePromise(myPopup.id, "cancel");
-	console.log("12. This is rv: " + rv);
+	let pw = await popupClosePromise(myPopup.id);
+	console.log("15. This is pw: " + pw);
+	//this works when uncommented.
+	console.log("16. Encrypting now! " + sjcl.encrypt(pw, details.body));
+	let newBody = await sjcl.encrypt(pw, details.body);
+	//newBody is ok.
+	console.log("17. newBody is " + newBody);
+
+	//this is a promise object
+	console.log(" current id? " + await messenger.compose.getComposeDetails(myPopup.id));
+	//another promise object 
+	console.log(" what's this? " + await messenger.compose.setComposeDetails(myPopup.id, { body: newBody }));
+
+	//Ok this is a promise!
+	//console.log("my compose details: " + messenger.compose.getComposeDetails(myPopup.id));
+	
+	
 }
 
 async function getBodyText(tab) {
@@ -96,6 +127,7 @@ async function getBodyText(tab) {
 	//setting details
 	details = await messenger.compose.getComposeDetails(tab.id);
 	console.log("4. This is the body text from the getBodyText function: " + details.body);
+	
 }
 
 
--- CryptoAdd-on/popup.js
+++ CryptoAdd-on/popup.js
@@ -1,8 +1,7 @@
 /*
 This Popup handles the input of the password, and two buttons: encrypt and cancel.
 */
-console.log("Popup.js is loaded.")
-var password;
+console.log("7. Popup.js is loaded.")
 
 window.addEventListener("load", onLoad);
 //this is loaded, each time the Insecure button is clicked.
@@ -11,32 +10,30 @@ async function notifyMode(event) {
 
 
     //this works, why can't I send it to background??
-    //var password = await document.getElementById("password_input").value;
-    console.log("6. This gets the password " + password);
+    var password = await document.getElementById("password_input").value;
+    console.log("9. This gets the password " + password);
+    console.log("9.1 " + password);
+    await messenger.runtime.sendMessage({ popupCloseMode: password })
 
-    //Ah! this is the data (encrypt or cancel!)
-    console.log("7. An 'encrypt' or 'Cancel' has pressed: " + event.target.getAttribute("data"));
+    //Ah! this send the data to the popupCloseMode function
+    
+    //ok password works with above...but then this breaks, so need 2!
+    console.log("10. An 'encrypt' or 'Cancel' has pressed: " + event.target.getAttribute("data"));
     await messenger.runtime.sendMessage({ popupCloseMode: event.target.getAttribute("data") });
     
     //await - waits for a promise to fulfill
+    //ok this breaks!
     let win = await messenger.windows.getCurrent();
     messenger.windows.remove(win.id);
 
 }
 
-async function updatePassword() {
-    password = document.getElementById("password_input").value;
-    //this should be an object Promise?
-    await password;
-    console.log("is this a promise? " + password);
-}
-
 async function onLoad() {
     document.getElementById("button_ok").addEventListener("click", notifyMode);
     document.getElementById("button_cancel").addEventListener("click", notifyMode);
-    document.getElementById("password_input").addEventListener("blur", updatePassword);
+    document.getElementById("password_input").addEventListener("blur", notifyMode);
     //the page is loaded, on the "insecure" click.
-    console.log("5. popup.js onLoad is activated.");
+    console.log("8. popup.js has been actived by 'onLoad'.");
 }
 
 
