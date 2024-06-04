8e9b4a2609490218cc6513c2b5d43f3d85701092
drexl <drexl@little-beak.com>
2024-06-04 10:02:38 +0200

    code cleanup

--- Add-on/background.js
+++ Add-on/background.js
@@ -83,21 +83,33 @@ async function decryptionPopup() {
 					if (closingDecryptionPopup === "cancel" || closingDecryptionPopup === undefined) {
 
 					} else {
+						//this is gonna be all crap. it has to be done in the content-script.
+						//BUT, I need to send the password!
 						console.log("what is closingDecryptionPopup? " + closingDecryptionPopup);
 						console.log("We doing work now...");
 						//Have password -> add text from above, and decrypt
-						let password = closingDecryptionPopup;
+						let password = "cats";
 						//where did I get details.body from?
 						//can I get the body in here?
-						let oldBody = 
-						//let newBody = sjcl.decrypt(password, oldBody);
-						console.log("Message Body is: " + oldBody);
-						//console.log("newBody is: " + newBody);
-
-
+						
 						console.log("Inside decryption loop, password: " + password);
+
+						let oldBody2 = '{"iv":"7/yTUj7X+qUvXCwzlgTvGQ==","v":1,"iter":10000,"ks":128,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"UXJXd14GVhM=","ct":"1/bnGAHgrOv0Sd3/UE0fzUILPvORW0aWUgwvMFlvTDD77ARUyrtJRBwbxeJHJGvxdcXLilxwhvqu5/7tiARzUsWZd20U/GASRPP0SJrQ0yhIa3pyKnG+Uce3nex/glFuYTnMwDvzTBqsxN/sKh13vktNlvyiHmjxXZl42a8weXhTvF+0HFsSi30NoacXlHwyc00lGkTpFi6A4kg="}';
+						console.log("Inside decryption loop, Message Body is: " + oldBody2);
+						
+						let newBody2 = sjcl.decrypt(password, oldBody2);
+						console.log("what is a newBody2?" + newBody2);
+						console.log("Inside decryption loop, newBody is: " + newBody2);
+						//leave newBody2 as is...send it to the message content script.
 						//TODO: get document.body "text" - need to use the content scripts. meh
 						
+						//this gets us some script tag??
+						//msgBody = document.getElementsByTagName("body")[0].innerHTML;
+						
+						//not sure any of this shit is useful.
+						//console.log("msgBody is: " + msgBody);
+  						//let oldBody = msgBody[0].innerText;
+						//console.log("Oldbody from background is: " + oldBody);
 						//TODO: replace the 'new' decrypted text into the document body.
 
 					};
@@ -129,46 +141,6 @@ async function decryptionPopup() {
 
 
 //TODO Where I am at: the content script is 100% not understood, and probably mostly wrong. fix, step by step.
-/**
- * Use the startup phase to tell Thunderbird that it should load
- * our message-content-script.js file whenever a message is displayed
- */
-// const handleStartup = () => {
-// messenger.messageDisplayScripts.register({
-// 	js: [{ file: "messageContentScripts/message-content-script.js" }],
-// 	//will delete the following soon, don't need it.
-// 	//css: [{ file: "messageContentScripts/message-content-styles.css" }],
-// });
-// };
-
-/**
- * command handler: handles the commands received from the content script
- */
-// const doHandleCommand = async (message, sender) => {
-// const { command } = message;
-// const {
-// 	tab: { id: tabId },
-// } = sender;
-
-// console.log("Inside doHandleCommand! Woo!");
-
-// };
-
-// /**
-//  * handle the received message by filtering for all messages
-//  * whose "type" property is set to "command".
-//  */
-// const handleMessage = (message, sender, sendResponse) => {
-// if (message && message.hasOwnProperty("command")) {
-// 	// if we have a command, return a promise from the command handler
-// 	console.log("Inside handleMessage. Woo!!");
-// 	console.log("anything? " + getNotificationsDetails);
-// 	return doHandleCommand(message, sender);
-// }
-// };
-
-
-/** Adding all handlers here */
 
 messenger.composeAction.onClicked.addListener(encryptionPopup);
 messenger.messageDisplayAction.onClicked.addListener(decryptionPopup);
@@ -176,30 +148,6 @@ messenger.messageDisplayAction.onClicked.addListener(decryptionPopup);
 //this really only works from the compose window. busted.
 messenger.composeAction.onClicked.addListener(getBodyTextFromComposeWindow);
 
-// /**
-//  * Add a handler for communication with other parts of the extension
-//  *
-//  * 👉 There should be only one handler in the background script
-//  *    for all incoming messages
-//  */
-// messenger.runtime.onMessage.addListener(handleMessage);
-
-// /**
-//  * Execute the startup handler whenever Thunderbird starts
-//  */
-// document.addEventListener("DOMContentLoaded", handleStartup);
-
-
-// background-script.js
-// function handleMessage(request, sender, sendResponse) {
-// 	console.log(`A content script sent a message: ${request.greeting}`);
-// 	//1. We are getting inside here, but not getting a response!
-// 	//console.log("A content script sent a message: " + request.);
-// 	sendResponse({ response: "Response from background script" });
-// 	//return Promise.resolve({response: "response from background script."});
-//   }
-  
-// browser.runtime.onMessage.addListener(handleMessage);
 
 /**
  * Add a handler for the communication with other parts of the extension,
@@ -228,9 +176,12 @@ messenger.runtime.onMessage.addListener((message, sender, sendResponse) => {
 async function commandHandler(message, sender) {
     // Get the message currently displayed in the sending tab, abort if
     // that failed.
+	//const msgBody = await messenger.messages.getFull(sender.tab.id);
     const messageHeader = await messenger.messageDisplay.getDisplayedMessage(
         sender.tab.id
     );
+	
+	
 
     if (!messageHeader) {
         return;
@@ -238,18 +189,35 @@ async function commandHandler(message, sender) {
 
     // Check for known commands.
     switch (message.command) {
-        // case "getBannerDetails":
-        //     // Create the information we want to return to our message display
-        //     // script.
-        //     return { text: `Mail subject is "${messageHeader.subject}"` };
-		case "sayHi":
-			return { text: console.log("HI") };
-        // case "markUnread":
-        //     // Mark the message as unread.
-        //     messenger.messages.update(messageHeader.id, {
-        //         read: false,
-        //     });
-        // break;
+		case "sayHello":
+			//let dog = "Dog is a variable from the background-page.";
+			return { text: newBody2};
     }
 }
+// background.js
+let activeTabId; // Store the active tabId
+
+// Listen for tab activation
+browser.tabs.onActivated.addListener((activeInfo) => {
+  activeTabId = activeInfo.tabId;
+});
+
+// You can also listen for tab creation
+browser.tabs.onCreated.addListener((tab) => {
+  // Store the tabId here if needed
+});
+
+// background.js
+function injectContentScript() {
+	// Check if there's an active tabId
+	if (activeTabId) {
+	  // Execute content script in the active tab with the tabId
+	  browser.tabs.executeScript(activeTabId, {
+		file: "content.js" // Replace with your content script filename
+	  });
+	}
+  }
+  
+  // Example: Execute content script when the extension icon is clicked
+  browser.browserAction.onClicked.addListener(injectContentScript);
   
